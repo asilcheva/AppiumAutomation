8af75509db22310ad6a6e6e449ecc4eb5c0506c0
Anna <silcheva.anna@gmail.com>
2020-12-16 16:18:08 +0200

    Ex4*: Тест: проверка слов в поиске

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -49,11 +49,6 @@ public class FirstTest {
         return actualText.equals(expectedText);
     }
 
-    private WebElement waitForElementPresent(By by, String errorMessage, long timeout) {
-        WebDriverWait webDriverWait = new WebDriverWait(driver, timeout);
-        webDriverWait.withMessage(errorMessage);
-        return webDriverWait.until(ExpectedConditions.presenceOfElementLocated(by));
-    }
     @Test
     public void testSearchCancellation() {
         waitForElementPresentAndClick(By.id("org.wikipedia:id/fragment_onboarding_skip_button"), "Can't find Skip button", 10);
@@ -71,14 +66,36 @@ public class FirstTest {
         webDriverWait.withMessage(errorMessage);
         return webDriverWait.until(ExpectedConditions.invisibilityOfElementLocated(by));
     }
+
+    @Test
+    public void testSearchWordInResults() {
+        waitForElementPresentAndClick(By.id("org.wikipedia:id/fragment_onboarding_skip_button"), "Can't find Skip button", 10);
+        waitForElementPresentAndClick(By.id("org.wikipedia:id/search_container"), "Can't find Search Wikipedia input", 10);
+        waitForElementPresentAndSandKeys(By.id("org.wikipedia:id/search_src_text"), "Java", "Can't find search input", 10);
+        waitForElementPresent(By.id("org.wikipedia:id/page_list_item_title"), "", 10);
+        List<WebElement> list = driver.findElements(By.id("org.wikipedia:id/page_list_item_title"));
+        for (WebElement element : list) {
+            Assert.assertTrue(element.getAttribute("text").contains("Java"));
+        }
+    }
+
     private WebElement waitForElementPresentAndSandKeys(By by, String value, String errorMessage, long timeout) {
         WebElement resultElement = waitForElementPresent(by, errorMessage, timeout);
         resultElement.sendKeys(value);
         return resultElement;
     }
+
     private WebElement waitForElementPresentAndClick(By by, String errorMessage, long timeout) {
         WebElement resultElement = waitForElementPresent(by, errorMessage, timeout);
         resultElement.click();
         return resultElement;
     }
+
+    private WebElement waitForElementPresent(By by, String errorMessage, long timeout) {
+        WebDriverWait webDriverWait = new WebDriverWait(driver, timeout);
+        webDriverWait.withMessage(errorMessage);
+        return webDriverWait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+
 }
